cfc93cb952df6abd34e5193088a4188b5f70d823
shahd <shahdg44@gmail.com>
2024-11-05 23:12:53 +0200

    edited an error

--- shahdVector/shahdVector.cpp
+++ shahdVector/shahdVector.cpp
@@ -1,31 +1,6 @@
 #include <bits/stdc++.h>
-#include "implementation.h"
+#include "implementation.hpp"
 using namespace std;
-
-template <typename T>
-class shahdVector
-{
-private:
-    T *vector = nullptr;
-    int size;
-    int capacity;
-    void expandCapacity();
-
-public:
-    shahdVector(int size = 0) : size(size)
-    {
-        if (size < 0)
-            size = 1;
-        capacity = size + 10;
-        vector = new T[capacity]{};
-    }
-
-    ~shahdVector()
-    {
-        delete[] vector;
-        vector = nullptr;
-    }
-};
 
 int main()
 {
